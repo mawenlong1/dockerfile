41f36d8e31881f901b252ebff69f1a046c1db209
mawenlong <mwljndx@163.com>
2019-05-03 21:25:49 +0800

    dockerfile

--- mysql/schema.sql
+++ mysql/schema.sql
@@ -1,82 +1,18 @@
 -- 创建数据库
 create database `docker_order` default character set utf8 collate utf8_general_ci;
 
-use docker_order;
+use docker;
 
 -- 类目
-create table `product_category` (
-    `category_id` int not null auto_increment,
-    `category_name` varchar(64) not null comment '类目名字',
-    `category_type` int not null comment '类目编号',
+create table `product` (
+    `id` int not null auto_increment,
+    `name` varchar(64) not null,
+    `type` int not null x,
     `create_time` timestamp not null default current_timestamp comment '创建时间',
     `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (`category_id`),
-    unique key `uqe_category_type` (`category_type`)
+    primary key (`id`)
 )default charset utf8 collate utf8_unicode_ci;
-INSERT INTO `product_category` (`category_id`, `category_name`, `category_type`, `create_time`, `update_time`)
+INSERT INTO `product` (`id`, `name`, `type`, `create_time`, `update_time`)
 VALUES
 	(1,'热榜',11,'2017-03-28 16:40:22','2017-11-26 23:39:36'),
 	(2,'好吃的',22,'2017-03-14 17:38:46','2017-11-26 23:39:40');
-
--- 商品
-create table `product_info` (
-    `product_id` varchar(32) not null,
-    `product_name` varchar(64) not null comment '商品名称',
-    `product_price` decimal(8,2) not null comment '单价',
-    `product_stock` int not null comment '库存',
-    `product_description` varchar(64) comment '描述',
-    `product_icon` varchar(512) comment '小图',
-    `product_status` tinyint(3) DEFAULT '0' COMMENT '商品状态,0正常1下架',
-    `category_type` int not null comment '类目编号',
-    `create_time` timestamp not null default current_timestamp comment '创建时间',
-    `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (`product_id`)
-)default charset utf8 collate utf8_unicode_ci;
-INSERT INTO `product_info` (`product_id`, `product_name`, `product_price`, `product_stock`, `product_description`, `product_icon`, `product_status`, `category_type`, `create_time`, `update_time`)
-VALUES
-	('157875196366160022','皮蛋粥',0.01,39,'好吃的皮蛋粥','//fuss10.elemecdn.com/0/49/65d10ef215d3c770ebb2b5ea962a7jpeg.jpeg',0,1,'2017-03-28 19:39:15','2017-07-02 11:45:44'),
-	('157875227953464068','慕斯蛋糕',10.90,200,'美味爽口','//fuss10.elemecdn.com/9/93/91994e8456818dfe7b0bd95f10a50jpeg.jpeg',1,1,'2017-03-28 19:35:54','2017-04-21 10:05:57'),
-	('164103465734242707','蜜汁鸡翅',0.02,982,'好吃','//fuss10.elemecdn.com/7/4a/f307f56216b03f067155aec8b124ejpeg.jpeg',0,1,'2017-03-30 17:11:56','2017-06-24 19:20:54');
-
-
--- 订单
-create table `order_master` (
-    `order_id` varchar(32) not null,
-    `buyer_name` varchar(32) not null comment '买家名字',
-    `buyer_phone` varchar(32) not null comment '买家电话',
-    `buyer_address` varchar(128) not null comment '买家地址',
-    `buyer_openid` varchar(64) not null comment '买家微信openid',
-    `order_amount` decimal(8,2) not null comment '订单总金额',
-    `order_status` tinyint(3) not null default '0' comment '订单状态, 默认为新下单',
-    `pay_status` tinyint(3) not null default '0' comment '支付状态, 默认未支付',
-    `create_time` timestamp not null default current_timestamp comment '创建时间',
-    `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (`order_id`),
-    key `idx_buyer_openid` (`buyer_openid`)
-);
-
--- 订单商品
-create table `order_detail` (
-    `detail_id` varchar(32) not null,
-    `order_id` varchar(32) not null,
-    `product_id` varchar(32) not null,
-    `product_name` varchar(64) not null comment '商品名称',
-    `product_price` decimal(8,2) not null comment '当前价格,单位分',
-    `product_quantity` int not null comment '数量',
-    `product_icon` varchar(512) comment '小图',
-    `create_time` timestamp not null default current_timestamp comment '创建时间',
-    `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-    primary key (`detail_id`),
-    key `idx_order_id` (`order_id`)
-);
-
-create table `user_info`(
-  `id` varchar(32) not null ,
-  `username` varchar(32) default '',
-  `password` varchar(32) default '',
-  `openid` varchar(32) default '',
-  `role` tinyint(1) not null ,
-  `create_time` timestamp not null default current_timestamp ,
-  `update_time` timestamp  not null default current_timestamp on update current_timestamp ,
-  primary key (`id`)
-);
--- redis/README.md
+++ redis/README.md
@@ -1,3 +1,4 @@
 docker build -t test/redis .
 docker run -d -p 6379:6379 --name test-redis test/redis
 
+docker exec -it 33se /bin/bash 
